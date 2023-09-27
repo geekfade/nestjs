730546d3c09ca28f284c3d1a74c3aafb567905f3
geekfade <1747647718@qq.com>
2023-09-27 18:02:22 +0800

    🎨 nestJs-前端示例

---
+++ nestjs-front-end/src/store/index.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import { createPinia } from 'pinia';
+import { App } from 'vue';
+
+const store = createPinia();
+
+export function setupStore(app: App<Element>) {
+  app.use(store);
+}
+
+export { store };
---
+++ nestjs-front-end/src/views/login.vue
@@ -0,0 +1,12 @@
+<!-- @format -->
+
+<script setup lang="ts"></script>
+<template>
+    <!-- 注册 -->
+
+    <div>
+        
+    </div>
+
+</template>
+<style lang="scss" scoped></style>
