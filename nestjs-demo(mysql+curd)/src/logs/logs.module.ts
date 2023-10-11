c270ee28ef935c5bbdb3bd8eb68833a67ae55b92
geekfade <1747647718@qq.com>
2023-10-11 09:10:32 +0800

    📝 增加logs目录

---
+++ nestjs-demo(mysql+curd)/src/logs/logs.module.ts
@@ -0,0 +1,55 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { WinstonModule, WinstonModuleOptions, utilities } from 'nest-winston';
+import { LogEnum } from '../enum/config';
+import * as winston from 'winston';
+import { Console } from 'winston/lib/winston/transports';
+import { LogsService } from './logs.service';
+import { LogsController } from './logs.controller';
+
+function createDailyRotateTransport(level: string, filename: string) {
+  return new winston.transports.DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
+
+@Module({
+  imports: [
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const consoleTransport = new Console({
+          level: 'info',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike(),
+          ),
+        });
+        return {
+          transports: [
+            consoleTransport,
+            ...(configService.get(LogEnum.LOG_ON)
+              ? [
+                  createDailyRotateTransport('info', 'application'),
+                  createDailyRotateTransport('error', 'error'),
+                ]
+              : []),
+          ],
+        } as WinstonModuleOptions;
+      },
+    }),
+  ],
+  controllers: [LogsController],
+  providers: [LogsService],
+})
+export class LogsModule {}
