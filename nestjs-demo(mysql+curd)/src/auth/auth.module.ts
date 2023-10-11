3018c2393b79af60d9c7cea17a94a70b4bd6aacd
geekfade <1747647718@qq.com>
2023-10-11 18:04:43 +0800

    💄 实现jwt

--- nestjs-demo(mysql+curd)/src/auth/auth.module.ts
+++ nestjs-demo(mysql+curd)/src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: '',
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
